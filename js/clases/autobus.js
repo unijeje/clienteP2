eb861caba4e98d268410bea4dbf00f4591666f92
fmenacho <35448982+fmenacho@users.noreply.github.com>
2018-01-15 13:56:17 +0100

    Add files via upload

--- js/clases/autobus.js
+++ js/clases/autobus.js
@@ -1,8 +1,28 @@
 
 //objeto
-function Autobus()
+class Autobus
 {
-    //atributos
+	constructor(sMatricula,iNumAsientos,sModelo,iConsumo)
+	{
+	//atributos
+		this.matricula=sMatricula;
+		this.asientos=iNumAsientos;
+		this.modelo=sModelo;
+		this.consumo=iConsumo;
+		this.itv=false;
+		this.estado=true; //para saber si esta o no de baja
+	}
+
+    //funciones
+    pasarRevision()
+    {
+    	this.itv=true;
+    }
+
+    darBaja()
+    {
+    	this.estado=false;
+    }
+
 }
 
-//funciones
